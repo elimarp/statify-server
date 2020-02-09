b7778127ad0a19a9de93dc0cc3e3a963182c13fa
elimarp <epproducoes2015@gmail.com>
2020-02-09 11:33:25 -0300

    dist removed

--- dist/utils/consts.js
+++
@@ -1,6 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});exports. default = {
-    SPOTIFY_BASE_URL: 'https://api.spotify.com/v1',
-    STANDARD_RESPONSES: {
-        INTERNAL_ERROR: { success: false, message: 'Internal server error.' },
-    },
-}
